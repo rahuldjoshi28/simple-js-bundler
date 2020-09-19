a99b4f555f956e4f3186697283a9c9e5c948fcee
Rahul Joshi <rahul.joshi1@thoughtworks.com>
2020-09-19 19:40:24 +0530

    [Rahul] Add babel core transform for all file node imports

--- src/graph.js
+++ src/graph.js
@@ -1,4 +1,5 @@
 const { astFrom } = require("./ast");
+const { transformFromAst } = require("@babel/core");
 
 const { fileContents, getFilePath } = require("./file");
 const traverse = require("@babel/traverse").default;
@@ -6,15 +7,19 @@ const traverse = require("@babel/traverse").default;
 const graph = {};
 
 function graphFrom(ast, basePath) {
-  console.log({ basePath });
   traverse(ast, {
     ImportDeclaration: (importNode) => {
       const sourcePath = importNode.node.source.value + ".js";
       const { fullPath, dirPath } = getFilePath(basePath, sourcePath);
       const contents = fileContents(basePath, sourcePath);
-      graph[fullPath] = contents;
+      const ast1 = astFrom(contents);
 
-      graphFrom(astFrom(contents), dirPath);
+      const { code } = transformFromAst(ast1, null, {
+        presets: ["env"],
+      });
+      graph[fullPath] = code;
+
+      graphFrom(ast1, dirPath);
     },
   });
 
